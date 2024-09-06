d46e5489fedcc64ead82735841510a3d5f876757
Camille <camille.pellat@gmail.com>
2024-09-06 12:09:53 +0200

    "bip boop"

--- src/claire.py
+++ src/claire.py
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
-aaaaa
-=======
 a, b = 0, 5
 
 for i in range(b):
     print(" " * a, end="")
     a += 1
     print("Bonjour Claire")
->>>>>>> e46802aae19da7508c6123737eb1b03a52f6aa00

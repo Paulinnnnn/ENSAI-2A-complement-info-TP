d9606e1ad13b340c8abcea2a60fa5989a08d4e5b
Borrelli Claire <claire.borrelli@eleve.ensai.fr>
2024-09-06 12:34:54 +0200

    disrespect

--- src/Andre.py
+++ src/Andre.py
@@ -3,12 +3,4 @@ a, b = 0, 5
 for i in range(b):
     print(" " * a, end="")
     a += 1
-<<<<<<< HEAD
-<<<<<<< HEAD
-    print("Bonjour gros nul")
-=======
-    print("Bonjour gros naze")
->>>>>>> bfabe35807c17e4528cc2cdc1be8dfb3e3476248
-=======
-    print("Bonjour sale gros")
->>>>>>> d319c523c7b90a8ed354ab8652ba984fc421394d
+    print("Bonjour sale gros nul")

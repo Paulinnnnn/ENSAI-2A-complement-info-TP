e2d66b1fdd769fdf2b7e723412e9a9c7d51ea839
Paulin Lemoine <paulin.lemoine@eleve.ensai.fr>
2024-09-13 12:08:17 +0200

    delete_self

--- src/business_object/pokemon/Defender.py
+++ src/business_object/pokemon/Defender.py
@@ -3,7 +3,7 @@ from business_object.pokemon.abstract_pokemon import AbstractPokemon
 
 class Defender(AbstractPokemon):
     def __init__(self, stat_max=None, stat_current=None, level=0, name=None, type_pk=None):
-        super().__init__(self, stat_max, stat_current, level, name, type_pk)
+        super().__init__(stat_max, stat_current, level, name, type_pk)
 
     def get_pokemon_attack_coef(self):
         return 1 + (self.attack_current + self.defense_current) / 200

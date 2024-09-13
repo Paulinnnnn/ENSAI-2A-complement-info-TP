870e5f2d4503c8c4e604c394d7879a29809c6df9
Paulin Lemoine <paulin.lemoine@eleve.ensai.fr>
2024-09-13 11:05:07 +0200

    Create_attack_coef

--- src/business_object/pokemon/Attacker.py
+++ src/business_object/pokemon/Attacker.py
@@ -4,3 +4,6 @@ from abstract_pokemon import AbstractPokemon
 class Attacker(AbstractPokemon):
     def __init__(self):
         super().__init__()
+
+    def get_pokemon_attack_coef(self):
+        return 1 + (self.speed_current + self.attack_current) / 200

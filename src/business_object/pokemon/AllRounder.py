767fe69eaf274b3a499c5a70f00909f039201706
Paulin Lemoine <paulin.lemoine@eleve.ensai.fr>
2024-09-13 10:55:58 +0200

    add classes

---
+++ src/business_object/pokemon/AllRounder.py
@@ -0,0 +1,6 @@
+from abstract_pokemon import AbstractPokemon
+
+
+class AllRounder(AbstractPokemon):
+    def __init__(self):
+        super().__init__()
---
+++ src/business_object/pokemon/Defender.py
@@ -0,0 +1,6 @@
+from abstract_pokemon import AbstractPokemon
+
+
+class Defender(AbstractPokemon):
+    def __init__(self):
+        super().__init__()

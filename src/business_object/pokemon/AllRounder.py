67605371923dd0d03f0dacd1944887dbbc934027
Paulin Lemoine <paulin.lemoine@eleve.ensai.fr>
2024-09-13 11:07:23 +0200

    add_atcks

--- src/business_object/pokemon/AllRounder.py
+++ src/business_object/pokemon/AllRounder.py
@@ -4,3 +4,6 @@ from abstract_pokemon import AbstractPokemon
 class AllRounder(AbstractPokemon):
     def __init__(self):
         super().__init__()
+
+    def get_pokemon_attack_coef(self):
+        return 1 + (self.sp_atk_current + self.sp_def_current) / 200
--- src/business_object/pokemon/Defender.py
+++ src/business_object/pokemon/Defender.py
@@ -4,3 +4,6 @@ from abstract_pokemon import AbstractPokemon
 class Defender(AbstractPokemon):
     def __init__(self):
         super().__init__()
+
+    def get_pokemon_attack_coef(self):
+        return 1 + (self.attack_current + self.defense_current) / 200

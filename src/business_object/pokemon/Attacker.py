dd76935dcac53a51c045ac07defc992acdde32dc
Paulin Lemoine <paulin.lemoine@eleve.ensai.fr>
2024-09-13 12:01:06 +0200

    init

--- src/business_object/pokemon/Attacker.py
+++ src/business_object/pokemon/Attacker.py
@@ -2,7 +2,7 @@ from business_object.pokemon.abstract_pokemon import AbstractPokemon
 
 
 class Attacker(AbstractPokemon):
-    def __init__(self):
+    def __init__(self, stat_max=None, stat_current=None, level=0, name=None, type_pk=None):
         super().__init__()
 
     def get_pokemon_attack_coef(self):
